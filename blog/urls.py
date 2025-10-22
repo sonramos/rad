8d04da942a1a599cb711e20e38a3be2c234ffc0f
Jackson Ramos <jackson.ramoosg@gmail.com>
2025-10-22 15:56:32 -0300

    feature: conditions and loops template routes

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.hello, name="hello"),
     path("eco/<str:text>", views.eco),
-    path("info", views.api_info)
+    path("info", views.api_info),
+    path("conditions", views.conditions),
+    path("loops", views.loops),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from datetime import date
 
 def hello(request):
-    return HttpResponse("Bem-vindo ao meu blog!")
+    contexto = {
+        "usuario": "sonramos",
+        "frutas": ["pera", "laranja", "uva"],
+        "data_atual": date.today(),
+        "numero": 37,
+    }
+    return render(request, "blog/hello.html", contexto)
 
 def eco(request, text):
     return HttpResponse(f"Você digitou: {text}")
@@ -14,3 +21,19 @@ def api_info(request):
      "semestre": "2025.2"
   }
     return JsonResponse(info)
+
+def conditions(request):
+    contexto = {
+        "is_logged_in": True,
+        "age": 31,
+        "role": "admin",
+    }
+    return render(request, "blog/conditions.html", contexto)
+
+def loops(request):
+    contexto = {'produtos': [
+        {'nome': 'Notebook', 'preco': 3500},
+        {'nome': 'Mouse', 'preco': 80},
+        {'nome': 'Teclado', 'preco': 150},
+    ]}
+    return render(request, 'loops.html', contexto)
