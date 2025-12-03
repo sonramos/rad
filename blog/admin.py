f9dd10dc31e05bb918731914fa2d78299eee3b14
Jackson Ramos <jackson.ramoosg@gmail.com>
2025-12-03 16:17:11 -0300

    feat: models, migration e gerenciamento por admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Publisher, Author, Book, BookAuthor
 # Register your models here.
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'isbn', 'title', 'release_date', 'price', 'stock', 'publisher')
+    search_fields = ('title', 'isbn')
+    list_filter = ('publisher', 'release_date')
+
+@admin.register(BookAuthor)
+class BookAuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'author')
+    search_fields = ('book__title', 'author__name')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, null=False, blank=False, unique=True)
+    title = models.CharField(max_length=255)
+    release_date = models.DateField(null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.RESTRICT)
+    authors = models.ManyToManyField(Author)
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT)
+    author = models.ForeignKey(Author, on_delete=models.RESTRICT)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['book', 'author'], name='unique_book_author'
+            )
+        ]
